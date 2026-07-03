404d3fb399c7820cf15c0e37e1c67e7fe197b396
alexsoares <alexsaosilva@gmail.com>
2026-07-03 10:01:21 -0300

    Commit: problema de cors origin corrigido;

--- src/main/java/com/bookloop/security/SecurityConfig.java
+++ src/main/java/com/bookloop/security/SecurityConfig.java
@@ -101,7 +101,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsSource() {
         var config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://kehmmmut47.us-east-1.awsapprunner.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
--- target/generated-sources/annotations/com/bookloop/book/application/BookMapperImpl.java
+++ target/generated-sources/annotations/com/bookloop/book/application/BookMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-07-02T22:27:05-0300",
+    date = "2026-07-03T09:12:30-0300",
     comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.11 (Azul Systems, Inc.)"
 )
 @Component
